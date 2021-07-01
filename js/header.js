ffb19b360b6210a5582d9b31084ab8d10eeda4e6
FadingShadow <warsquad0@gmail.com>
2021-07-02 03:29:07 +0500

    Fix: Responsiveness across multiple screens
    Redefined css rules and made the website much more responsive. Now it will work on mobile, tablets and on large screens.

--- js/header.js
+++ js/header.js
@@ -1,4 +1,19 @@
 const header = document.querySelector("#header");
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("#nav");
+
+hamburger.addEventListener('click',() => {
+  hamburger.classList.toggle("close");
+  nav.classList.toggle("open-navbar");
+});
+
+nav.addEventListener("click", (e) =>{
+  if( nav.classList.contains("open-navbar") && e.target.classList.contains("nav-link")){
+    hamburger.classList.toggle("close");
+    nav.classList.toggle("open-navbar");
+  }
+})
+
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 50) {
     header.style.background = "rgb(13, 3, 23)";
