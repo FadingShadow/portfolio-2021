73e4d93e26ca0e0d2e8d78f8bb65e9773fa59c4a
FadingShadow <warsquad0@gmail.com>
2021-07-03 00:58:30 +0500

    Fix: Performance Improvement

--- js/animation.js
+++ js/animation.js
@@ -20,32 +20,39 @@ window.addEventListener("resize", () => {
   width -= box + 50;
 });
 
+let iter = 0;
+
 setInterval(function () {
   for (i = 0; i < 1; i++) {
-    let x = 0;
-    div[i] = document.createElement("div");
-    let y = Math.random();
-    div[i].setAttribute(
-      "style",
-      "width: " +
-        (y * box + box) +
-        "px; height: " +
-        (y * box + box) +
-        "px; top:  " +
-        Math.random() * height +
-        "px; left: " +
-        Math.random() * width +
-        "px;"
-    );
-
-    div[i].setAttribute("class", "box");
-    home.appendChild(div[i]);
-    setTimeout(
-      function (div) {
-        div.remove();
-      },
-      3000,
-      div[i]
-    );
+    if(iter < 6){
+      let x = 0;
+      div[i] = document.createElement("div");
+      iter++;
+      console.log(iter);
+      let y = Math.random();
+      div[i].setAttribute(
+        "style",
+        "width: " +
+          (y * box + box) +
+          "px; height: " +
+          (y * box + box) +
+          "px; top:  " +
+          Math.random() * height +
+          "px; left: " +
+          Math.random() * width +
+          "px;"
+      );
+  
+      div[i].setAttribute("class", "box");
+      home.appendChild(div[i]);
+      setTimeout(
+        function (div) {
+          div.remove();
+          iter--;
+        },
+        3000,
+        div[i]
+      );
+    }
   }
 }, 250);
